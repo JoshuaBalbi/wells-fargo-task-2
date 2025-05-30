ca298b7a96fd8beef09bafff8788f433872c0fae
JoshuaBalbi <Balbi.joshua@gmail.com>
2025-05-30 11:57:57 -0400

    Added Client, Portfolio, and Security Entities
    Added Client, Portfolio, and Security Entities

---
+++ src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
@@ -0,0 +1,45 @@
+package com.wellsfargo.counselor.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.JoinColumn;
+
+
+@Entity
+public class Portfolio {
+
+    @Id
+    @GeneratedValue()
+    private long portfolioId;
+
+    @OneToOne
+    @JoinColumn(name = "client_id", nullable = false, unique = true)
+    private Client client;
+
+
+    protected Portfolio() {
+
+    }
+
+    public Portfolio(Client client) {
+        this.client = client;
+    }
+
+
+    public long getPortfolioId() {
+        return portfolioId;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+
+}
